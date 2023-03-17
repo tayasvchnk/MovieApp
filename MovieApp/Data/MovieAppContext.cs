e4fe6f6a6865f024d09fc76d3ad99f83787e15ef
Taya <tkhurduikova@gmail.com>
2023-03-17 19:53:17 +0600

    Добавьте файлы проекта.

---
+++ MovieApp/Data/MovieAppContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieApp.Data
+{
+    public class MovieAppContext : DbContext
+    {
+        public MovieAppContext(DbContextOptions<MovieAppContext>options) : base(options) 
+        { 
+        }
+        public DbSet<Movie> Movie { get; set; } 
+        public DbSet<Category> Category { get; set; }
+    }
+}
